c9e6b1ce36255adce3f523435a4e5ec834bdddcf
unknown <cynoxsis@gmail.com>
2020-08-06 11:42:16 -0600

    another attempt server.js

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/dice-game'));
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/dice-game/app.component.html'));
+    }
+)
+app.get('/rules', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/dice-game/app.component.html'));
+    }
+)
+app.get('/game', function(req, res) {
     res.sendFile(path.join(__dirname +
         '/dist/dice-game/app.component.html'));
     }

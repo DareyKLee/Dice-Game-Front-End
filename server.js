5213b03e41a6636d22ad21d4e789580d99146007
unknown <cynoxsis@gmail.com>
2020-08-06 10:49:14 -0600

    attempting server.js routing fix

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/dice-game'));
+
+app.get('/game', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/dice-game/game.component.html'));
+    }
+)
+
+app.get('/rules', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/dice-game/rules.component.html'));
+    }
+)
+
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/dice-game/app.component.html'));})
-app.listen(process.env.PORT || 8080);        
+        '/dist/dice-game/app.component.html'));
+    }
+)
+
+app.listen(process.env.PORT || 8080);

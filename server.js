95f11039e8c59d73f2ea54fc4187d1e7882f171c
unknown <cynoxsis@gmail.com>
2020-08-06 12:43:18 -0600

    remove static server.js

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const STATIC = path.resolve(__dirname, 'dist');
-const APP = path.resolve(STATIC, '/*');
 
-app.use(express.static(STATIC));
+app.use(express(__dirname + '/dist/dice-game'));
 
-app.get('*', function(req, res) {
-    res.sendFile(APP);
-});
-
-// app.use(express.static(__dirname + '/dist/dice-game'));
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.join(__dirname +
-//         '/dist/dice-game/app.component.html'));})
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/dice-game/app.component.html'));
+    }
+);
 
 app.listen(process.env.PORT || 8080);        
